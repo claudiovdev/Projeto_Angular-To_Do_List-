824f502d20a87a79d46a563ced1b0f7f649d8fac
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-09-22 10:51:00 -0300

    Concluindo o gerenciador de taks

--- src/app/module/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/module/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emitItemTaksList = new EventEmitter();
+
+  public addItemTaskList: string = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public submitItemTaksList(){
+
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if(this.addItemTaskList){
+      this.emitItemTaksList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+    
   }
 
 }
--- src/app/module/home/components/todo-list/todo-list.component.ts
+++ src/app/module/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { first, last } from 'rxjs';
+
+//Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
+  public list: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]'); 
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
+
+
+  public excluirTaks(event: number){
+    this.list.splice(event, 1);
+  }
+
+  public excluirTodos(){
+    this.list = [];
+  }
+
+  public setEmiterTaksList(event: string){
+   this.list.push({task: event, checked: false})
+  }
+
+  public validationInput(event: string, index: number){
+
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, desejadeletar?");
+      if(confirm){
+        this.excluirTaks(index);
+      }
+    }
+  }
+
+  public setLocalStorage(){
+    if(this.list){
+      this.list.sort( (first, last) => Number (first.checked) - Number(last.checked))
+      localStorage.setItem("list", JSON.stringify(this.list))
+    }
+    
   }
 
 }
